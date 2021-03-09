1769cfb4f5352dfef859201181cc35ce20fa3de9
Pablo <Pablo@DESKTOP-2G46TPQ>
2021-03-09 15:19:07 +0100

    subir archivo al GitHUb

---
+++ src/ejercicio_git/Calculadora.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_git;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Calculadora {
+
+  private int num1;
+ 
+    private int num2;
+
+    
+    public Calculadora(int a, int b) {
+        num1 = a;
+        num2 = b;
+    }
+
+    public int suma() {
+        int result = num1 + num2;
+        return result;
+    }
+
+    public int resta() {
+        int result = num1 - num2;
+        return result;
+    }
+
+    public int multiplicar() {
+        int result = num1 * num2;
+        return result;
+    }
+
+    public int divide() {
+        int result = num1 / num2;
+        return result;
+    }
+
+}
